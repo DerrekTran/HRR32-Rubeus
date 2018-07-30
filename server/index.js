2b54d6a8fdbd1141ed733d080d5984749c29cbb8
Derrek Tran <blubby94@gmail.com>
2018-07-29 21:53:27 -0700

    feat/ working live chat

--- server/index.js
+++ server/index.js
@@ -4,16 +4,21 @@ const app = express();
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
-const utils = require('./utils.js')
-const db = require('../db/index.js');
-const apiHelpers = require('../lib/apiHelper.js');
-const dataHelpers = require('../lib/dataHelpers.js')
-const apiSearch = require('../lib/apiSearch.js');
-const config = require('../config/civic.js');
+const utils = require('./utils.js');
 const server = require('http').createServer(app);
 const socket = require('socket.io');
 const io = socket(server);
 
+//Database Dependences Below
+const db = require('../db/users.js');
+const townhalls = require('../db/townhalls.js');
+//**********************************************
+
+const apiHelpers = require('../lib/apiHelper.js');
+const dataHelpers = require('../lib/dataHelpers.js')
+const apiSearch = require('../lib/apiSearch.js');
+const config = require('../config/civic.js');
+const path = require("path");
 /************************************************
 Passport Related (Below)
 ************************************************/
@@ -56,7 +61,7 @@ const port = process.env.PORT || 3000;
 // app.listen(port, () => {
 //   console.log(`Hello from ${port}!`);
 // });
-
+//*********live-chat ***********//
 io.on('connection', (client) => {
   //emitting evnets to client
   console.log(client.id);
@@ -66,18 +71,10 @@ io.on('connection', (client) => {
     io.emit('receive_message', data);
   })
 });
-
-server.listen(port, () => {
+ server.listen(port, () => {
   console.log(`server listening from ${port}!`)
 });
-
 // ////******route requests*********///
-
-app.get('/login', (req, res, next) => {
-  console.log('login route called')
-  res.send('login page')
-})
-
 
 app.post('/login', (req, res, next) => {
   console.log(req.body);
@@ -88,12 +85,12 @@ app.post('/login', (req, res, next) => {
 app.post('/reps', (req, res, next) => {
 
   console.log("POST to /saveUser, req.body is", req.body);
-  const locater = req.body.zip ? req.body.zip
+  const locator = req.body.zip ? req.body.zip
                   : req.body.location ? dataHelpers.abbrState(req.body.location, 'name')
                   : null;
   const region = req.body.region;
 
-  apiSearch.searchByZip(locater, (response) => {
+  apiSearch.searchByZip(locator, (response) => {
     if (response.error) {
       console.log(response.error);
       res.send(JSON.stringify('Please enter valid ZIP code.'));
@@ -103,12 +100,11 @@ app.post('/reps', (req, res, next) => {
       res.send(apiHelpers.getOfficials(region, response));
     }
   });
-
-  // uday will add the thing here
-  // db.insertData(data, cb);
 });
 
-
+app.post('/townhall', utils.authCheck, (req, res, next) => {
+  console.log('req received')
+})
 /******************************************************************************
 Name: Passport Authentication Routes
 Description:   Will determine the routes to direct user to and send them back
@@ -120,12 +116,9 @@ app.get('/auth/google',
 app.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
-    console.log('hello you submitted!');
-    // res.send('you are now logged in');
-    console.log('req.user', req.user);
+    console.log('req.user is', req.user);
     res.redirect('/');
   });
-
 
 app.get('/test', utils.authCheck, (req, res) => {
   console.log('passed authCheck');
@@ -135,5 +128,111 @@ app.get('/test', utils.authCheck, (req, res) => {
 
 //Temp function for Mubeen front-end prior to RR implementation
 app.get('/checkuser', utils.authCheck, (req, res) => {
-  res.send('user exists');
+  console.log('passed authcheck')
+  req.user ? res.send(req.user) : res.send('whoops');
+});
+
+
+
+
+/******************************************************************************
+Name:  React Router Redirect Routes
+Description:  Implements the ability to directly access a component via url
+              (e.g. localhost:3000/someUrl)
+*******************************************************************************/
+
+
+app.get('/main', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  });
+});
+
+
+app.get('/townhall', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  });
+});
+
+
+app.get('/dashboard', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  });
+});
+
+
+app.get('/map', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  });
+});
+
+
+app.get('/login', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  });
+});
+
+
+app.get('/zip', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  });
+});
+
+/******************************************************************************
+Name:  TownHall Query Routes
+Description:  Implements the ability to access data from the database for the
+              townhall
+*******************************************************************************/
+app.get('/alltownhalls', function(req, res) {
+  console.log('alltownhalls GET')
+  townhalls.getNames()
+    .then(names => res.send(names))
+    .catch(err => {res.status(500).send(err)});
+});
+
+app.post('/create', (req, res) => {
+  //console.log('createtownhall posted, ', req.body.townHallName)
+  const townHallName = req.body.townHallName;
+  townhalls.createTownHall(townHallName)
+
+  res.send('createtown hall POST return')
+})
+
+app.post('/question', function(req, res) {
+  const question = String(req.body.question);
+  const hallName = String(req.body.townHallName);
+  //change the user ID to be from hard coded to user id
+  const userRowId = Number(req.body.userRowId);
+  //
+  console.log('question posted', req.body)
+  //
+  townhalls.createQuestion(question, userRowId, hallName).then(results => res.status(201).send(results));
+});
+
+app.get('/questions', function(req, res) {
+  const townHallName = req.query.townHall;
+
+    townhalls.getQuestions(townHallName)
+       .then(questions => {
+          console.log(questions);
+          res.send(questions)
+        })
+       .catch(() => res.status(500).send());
 });
